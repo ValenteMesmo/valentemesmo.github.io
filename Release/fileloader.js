09aaac8d50b852db159a9f5d3f05c9ccf2762d00
valentemesmo <valentemesmo@gmail.com>
2015-06-28 20:09:16 -0300

    Updating game version

--- Release/fileloader.js
+++ Release/fileloader.js
@@ -26,8 +26,8 @@ Module.expectedDataFileDownloads++;
     var REMOTE_PACKAGE_NAME = typeof Module['locateFile'] === 'function' ?
                               Module['locateFile'](REMOTE_PACKAGE_BASE) :
                               ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
-    var REMOTE_PACKAGE_SIZE = 28055588;
-    var PACKAGE_UUID = '16f22c56-985c-4f6c-b5c0-84c6a50dc5b4';
+    var REMOTE_PACKAGE_SIZE = 28066236;
+    var PACKAGE_UUID = '90ab5689-7d1f-4f1d-ab2c-215058f70b9c';
   
     function fetchRemotePackage(packageName, packageSize, callback, errback) {
       var xhr = new XMLHttpRequest();
@@ -115,14 +115,14 @@ Module['FS_createPath']('/', 'Resources', true, true);
         this.requests[this.name] = null;
       },
     };
-      new DataRequest(0, 118680, 0, 0).open('GET', '/level0');
-    new DataRequest(118680, 144432, 0, 0).open('GET', '/mainData');
-    new DataRequest(144432, 144589, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
-    new DataRequest(144589, 167453, 0, 0).open('GET', '/sharedassets0.assets');
-    new DataRequest(167453, 25969973, 0, 0).open('GET', '/sharedassets1.assets');
-    new DataRequest(25969973, 25980076, 0, 0).open('GET', '/sharedassets1.resource');
-    new DataRequest(25980076, 27555112, 0, 0).open('GET', '/Resources/unity_default_resources');
-    new DataRequest(27555112, 28055588, 0, 0).open('GET', '/Resources/unity_builtin_extra');
+      new DataRequest(0, 122184, 0, 0).open('GET', '/level0');
+    new DataRequest(122184, 148200, 0, 0).open('GET', '/mainData');
+    new DataRequest(148200, 148357, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(148357, 171901, 0, 0).open('GET', '/sharedassets0.assets');
+    new DataRequest(171901, 25980621, 0, 0).open('GET', '/sharedassets1.assets');
+    new DataRequest(25980621, 25990724, 0, 0).open('GET', '/sharedassets1.resource');
+    new DataRequest(25990724, 27565760, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(27565760, 28066236, 0, 0).open('GET', '/Resources/unity_builtin_extra');
 
       var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
       var IDB_RO = "readonly";
